f77e583a81adf13d4a5d03dcba094b1592267a79
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2010-07-01 05:31:45 +0000

    added Peak_core_Codegen class for generating application code files deprecated baseurl() in Functions/general.php, use method baseUrl() from Peak_View_Render instead

--- trunk/library/Peak/Core/Codegen.php
+++ trunk/library/Peak/Core/Codegen.php
@@ -6,9 +6,90 @@
  */
 class Peak_Core_Codegen
 {
+	
+	//path where code sample file are
+	private $sample_path;
+		
+	
+	/**
+	 * Setup sample path
+	 */
+	public function __construct()
+	{
+		$this->sample_path = dirname(__FILE__).'/Codegen/';
+	}
+	
+	/**
+	 * Add data for sample(sample have method) and load simple file and return the content parsed
+	 *
+	 * @param string $file
+	 * @param array  $data
+	 */
+	public function genFromSample($sample, $data = null)
+	{
+		$sample_file = $this->sample_path.'/'.$sample.'.php';
+		if(file_exists($sample_file)) 
+		{
+			$method = $sample.'Sample';
+			if(method_exists($this, $method)) { 
+				$data = $this->$method($data);
+			}
+			
+			$content = include($sample_file);
+		}
+		else $content = false;
+		
+		return $content;		
+	}
+	
+	/**
+	 * Save generated sample code to a file
+	 *
+	 * @param string $sample
+	 * @param string $filepath
+	 * @param array/null $data
+	 * @param bool $overwrite
+	 * @return bool
+	 */
+	public function saveSample($sample, $filepath, $data = null, $overwrite = false)
+	{
+		$result = $this->genFromSample($sample,$data);
+		if(!$result) {
+			return false;
+		}
+		elseif(file_exists($filepath)) {
+			if($overwrite) { 			
+				if(file_put_contents($filepath,$result)) return true;
+			}
+
+		}
+		else {
+			if(file_put_contents($filepath,$result)) return true;
+			else return false;
+		}
+		return false;
+	}
     
-    public function ConfigsFileContent()
+    public function configsSample($data)
     {        
+    	$original_data = array('PROJECT_NAME'     => 'MyApp',
+    	                       'PROJECT_DESCR'    => '',
+    	                       'DEV_MODE'         => 'false',
+    	                       'APP_LANG'         => 'en',
+    	                       'APP_THEME'        => 'default',
+    	                       'APP_DEFAULT_CTRL' => 'indexController',
+    	                       'SVR_URL'          => 'http://127.0.0.1',
+    	                       'ROOT'             => '',
+    	                       'LIBRARY_ROOT'     => '',
+    	                       'APPLICATION_ROOT' => '',
+    	                       'ZEND_LIB_ROOT'    => '',
+    	                       'APP_LOGIN_NAME'   => '',
+    	                       'APP_LOGIN_PASS'   => '',
+    	                       'ENABLE_PEAK_CONTROLLERS' => 'false');
+    	                       
+        $data = array_merge((array)$original_data, (array)$data);
+        
+        return $data;
 
     }
     
