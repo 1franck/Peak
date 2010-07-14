f52645ac6a407fba4d4f9cea56d3ee0ffa8c9e1e
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2010-07-14 15:57:11 +0000

    added application tests folder

---
+++ tests/temps/application/default/bootstrap.php
@@ -0,0 +1,19 @@
+<?php 
+
+class bootstrap extends Peak_Bootstrap 
+{
+    
+    public function _initEnv()
+    {
+        date_default_timezone_set('America/Montreal');
+    }
+    
+    public function _initView()
+    {
+        $view = Peak_Registry::obj()->view;
+                
+        //Render view as layouts        
+        $view->setRenderEngine('Layouts');        
+    }
+    
+}
