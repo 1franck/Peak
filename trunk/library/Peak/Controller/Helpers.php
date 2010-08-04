57079cadb17364af15aebc0bdeeae76e39f914fa
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2010-08-04 05:21:55 +0000

    passed to version 0.7.97 some enhancements and changes to View/Controllers helpers

--- trunk/library/Peak/Controller/Helpers.php
+++ trunk/library/Peak/Controller/Helpers.php
@@ -6,51 +6,17 @@
  * @author   Francois Lajoie 
  * @version  $Id$ 
  */
-class Peak_Controller_Helpers
+class Peak_Controller_Helpers extends Peak_Helpers
 {
     
-    private $_objects = array();
-
-    /**
-     * Retreive objects, try to create object if not already setted
-     *
-     * @param  string $name
-     * @return object helper object
-     */
-	public function __get($name)
-	{
-		if(isset($this->_objects[$name])) return $this->_objects[$name];
-		else
-		{
-			$helper_name_prefix = 'ctrl_helper_';
-			$name = trim(stripslashes(strip_tags($name)));
-			$helper_file = CONTROLLERS_ABSPATH.'/helpers/'.$name.'.php';
-
-			$new_helper = $helper_name_prefix.$name;
-
-			if(!isset($this->_objects[$new_helper])) {
-				if(file_exists($helper_file)) {
-					include($helper_file);
-					$this->_objects[$name] = new $new_helper();
-				}
-				else {
-					throw new Peak_Exception('ERR_CTRL_HELPER_NOT_FOUND');
-				}
-			}
-			
-			return $this->_objects[$name];
-		}
-	}
-	
-	/**
-	 * Check if $_objects key name exists
-	 *
-	 * @param  string $object_name
-	 * @return bool
-	 */
-	public function __isset($object_name)
-	{
-		return (isset($this->_objects[$object_name])) ? true : false;
-	}
+    public function __construct()
+    {
+    	$this->_prefix    = 'Controller_Helper_';
+    	
+    	$this->_paths     = array(CONTROLLERS_HELPERS_ABSPATH,
+    			                  LIBRARY_ABSPATH.'/Peak/Controller/Helper');
+    			                  
+    	$this->_exception = 'ERR_CTRL_HELPER_NOT_FOUND';
+    }
     
 }
---
+++ trunk/library/Peak/Helpers.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Peak Abstract Helpers Object Containers
+ *  
+ * @author   Francois Lajoie 
+ * @version  $Id$ 
+ */
+abstract class Peak_Helpers
+{
+    
+	protected $_prefix    = '';
+	protected $_paths     = array();
+    protected $_objects   = array();
+    protected $_exception = 'ERR_DEFAULT';
+
+    /**
+     * Retreive objects, try to create object if not already setted
+     *
+     * @param  string $name
+     * @return object helper object
+     */
+	public function __get($name)
+	{
+		if(isset($this->_objects[$name])) return $this->_objects[$name];
+		else
+		{		
+			$name = trim(stripslashes(strip_tags($name)));
+			$helper_class_name = $this->_prefix.$name;
+
+			$file_found = false;
+			foreach($this->_paths as $k => $v)
+			{
+				$helper_file = $v.'/'.$name.'.php';
+				if(file_exists($helper_file)) {
+					$file_found = true;
+					break;
+				}
+			}
+
+			if($file_found) {
+				include($helper_file);
+				if(!class_exists($helper_class_name,false))	throw new Peak_Exception($this->_exception,$name);
+				$this->_objects[$name] = new $helper_class_name();
+				return $this->_objects[$name];
+			}
+			else throw new Peak_Exception($this->_exception,$name);
+		}
+	}
+	
+	/**
+	 * Check if $_objects key name exists
+	 *
+	 * @param  string $object_name
+	 * @return bool
+	 */
+	public function __isset($name)
+	{
+		return (isset($this->_objects[$name])) ? true : false;
+	}
+	
+	/**
+	 * Unset helper object
+	 *
+	 * @param string $name
+	 */
+	public function __unset($name)
+	{
+		if(array_key_exists($name,$this->_objects)) unset($this->_objects[$name]);
+	}
+	
+}
