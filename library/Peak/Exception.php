7340ef12f65bb88d1038493f09d5e69b0c790ae3
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2010-11-07 05:05:39 +0000

    Peak_Exception > deprecated method getDebugtrace(). Peak_Exception > removed property $_trace. Peak_Core > added set_exception_handler('pkexception') to handle all uncaught exceptions

--- library/Peak/Exception.php
+++ library/Peak/Exception.php
@@ -8,34 +8,25 @@
  */
 class Peak_Exception extends Exception
 {
-	
-	/**
-	 * Error file/line/code trace
-	 * @var array
-	 */
-    private $_trace;
-    
+	   
     /**
      * Error constant name
      * @var string
      */
 	private $_errkey;
     
-	//const ERR_APP_MOD_NOT_FOUND             = 'Application: module %1$s not found.'; //@deprecated
-	//const ERR_APP_MOD_NOT_SPECIFIED         = 'Application: module not specified.';  //@deprecated
+	/**
+	 * Errors messages
+	 */
 	const ERR_APP_CTRL_NOT_FOUND            = 'Application: controller %1$s not found.';
-	//const ERR_APP_LOGIN_CTRL_NOT_FOUND      = 'Application: controller loginController not found.';  //@deprecated
 	const ERR_ROUTER_URI_NOT_FOUND          = 'Router: request uri not found';
-	//const ERR_CORE_NO_CTRL_FOUND            = 'Core: application controllers not found.'; //@deprecated
 	const ERR_CORE_EXTENSION_NOT_FOUND      = 'Core: extension %1$s not found.';
-	//const ERR_HELPER_NOT_FOUND              = 'Helper: file %1$s not found.';  //@deprecated
 	const ERR_CTRL_ACTION_NOT_FOUND         = 'Controller: action \'%1$s\' not found in %2$s';
 	const ERR_CTRL_DEFAULT_ACTION_NOT_FOUND = 'Controller: no _index() method found.';
 	const ERR_CTRL_HELPER_NOT_FOUND         = 'Controller: helper \'%1$s\' not found.';
 	const ERR_VIEW_ENGINE_NOT_SET           = 'View rendering engine not set. Use setRenderEngine() from Peak_View before trying to render application controller.';
 	const ERR_VIEW_HELPER_NOT_FOUND         = 'View: helper \'%1$s\' not found.';
 	const ERR_VIEW_TPL_NOT_FOUND            = 'View: theme file %1$s not found.';
-	//const ERR_VIEW_LAYOUT_NOT_FOUND         = 'View: layout template file %1$s not found.'; //@deprecated
 	const ERR_DEFAULT                       = 'Request failed';
 		
 	
@@ -47,8 +38,6 @@ class Peak_Exception extends Exception
      */
     public function __construct($errkey = null, $infos = null)
 	{		    	    
-	    $this->_trace = array('file' => basename(parent::getFile()),'line' => parent::getLine(),'code' => parent::getCode());
-
 	    $this->_errkey = $errkey;    
 	    
 	    $message = $this->handleErrConstToText($errkey,$infos);	    
@@ -78,7 +67,7 @@ class Peak_Exception extends Exception
 	}
 
 	/**
-	 * Get debug trace of current exception
+	 * Get debug trace of current exception @deprecated
 	 *
 	 * @return string
 	 */
