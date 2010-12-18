acf5c0684fe84faa770d41967f1524e869801f3c
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2010-12-18 08:27:27 +0000

    moved string to array conversion from matchRegex to addRegex in Peak_Router updated RouterTest.php

--- tests/Peak/RouterTest.php
+++ tests/Peak/RouterTest.php
@@ -148,7 +148,98 @@ class Peak_RouterTest extends PHPUnit_Framework_TestCase
             return;
         }
         $this->fail('An expected exception has not been raised.');
+        
+        
 	}
-	    
+	
+	function testSimpleRegex1()
+	{
+		//add regex the standard way
+		$this->peakrouter->addRegex('custom_page', array('controller' => 'index', 'action' => 'news'));
+		
+		//fake $_SERVER['REQUEST_URI'];
+		$_SERVER['REQUEST_URI'] = '/custom_page';
+		
+		$this->peakrouter->getRequestURI();
+		//print_r($this->peakrouter);
+		
+		//request uri , controller , action
+		$this->assertTrue($this->peakrouter->request_uri === 'custom_page');
+		$this->assertTrue($this->peakrouter->controller === 'index');
+		$this->assertTrue($this->peakrouter->action === 'news');
+				
+		//params
+		$this->assertEmpty($this->peakrouter->params);
+		
+		//params assoc
+		$this->assertEmpty($this->peakrouter->params_assoc);
+	}
+	   
+	function testSimpleRegex2()
+	{
+		//add regex the short way with custom integer
+		$this->peakrouter->addRegex('post/(\d+)', 'blog.post');
+		
+		//fake $_SERVER['REQUEST_URI'];
+		$_SERVER['REQUEST_URI'] = '/post/89';
+		
+		$this->peakrouter->getRequestURI();
+		//print_r($this->peakrouter);
+		
+		//request uri , controller , action
+		$this->assertTrue($this->peakrouter->request_uri === 'post/89');
+		$this->assertTrue($this->peakrouter->controller === 'blog');
+		$this->assertTrue($this->peakrouter->action === 'post');
+		
+		//params
+		$this->assertTrue(is_array($this->peakrouter->params));
+		$this->assertTrue($this->peakrouter->params[0] == 89);
+		
+		//params assoc
+		$this->assertEmpty($this->peakrouter->params_assoc);
+		
+		
+		//retry but with invalid data at the end. the regex is supposed to fail and the normal request routing should happen
+		$_SERVER['REQUEST_URI'] = '/post/89/test1234';
+		
+		$this->peakrouter->getRequestURI();
+		//print_r($this->peakrouter);
+		
+		//request uri , controller , action
+		$this->assertTrue($this->peakrouter->request_uri === 'post/89/test1234');
+		$this->assertTrue($this->peakrouter->controller === 'post');
+		$this->assertTrue($this->peakrouter->action === '89');
+		
+		//params
+		$this->assertTrue(is_array($this->peakrouter->params));
+		$this->assertTrue($this->peakrouter->params[0] == 'test1234');
+		
+		//params assoc
+		$this->assertEmpty($this->peakrouter->params_assoc);
+		
+		return $this->peakrouter;
+	
+	}
+	
+	/**
+	 * @depends testSimpleRegex2
+	 */
+	function testDeleteRegex(Peak_Router $router)
+	{
+		$this->peakrouter = $router;
+			
+		$this->peakrouter->deleteRegex('post/(\d+)');
+		
+		//fake $_SERVER['REQUEST_URI'];
+		$_SERVER['REQUEST_URI'] = '/post/89';
+		
+		$this->peakrouter->getRequestURI();
+		//print_r($this->peakrouter);
+		
+		//request uri , controller , action
+		$this->assertTrue($this->peakrouter->request_uri === 'post/89');
+		$this->assertTrue($this->peakrouter->controller === 'post');
+		$this->assertTrue($this->peakrouter->action === '89');
+	}
     
 }
