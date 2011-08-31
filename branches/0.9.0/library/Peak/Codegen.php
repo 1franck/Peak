0631e8dc1cfac24d997476187250e1aecb733f99
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2011-08-31 01:48:57 +0000

    passed method generate() to abstract method in Peak_Codegen moved method getIndent() from Peak_Codegen_Class to Peak_Codegen fixed method generate() that was not using $_is_abstract property in Peak_Codegen_Class added property $_is_final and method isFinal() to Peak_Codegen_Class added .htaccess to demo/app folder

--- branches/0.9.0/library/Peak/Codegen.php
+++ branches/0.9.0/library/Peak/Codegen.php
@@ -23,6 +23,18 @@ abstract class Peak_Codegen
 	const PHP_OPEN_TAG = '<?php';
 	
 	/**
+	 * Get indentation string
+	 *
+	 * @param  integer $multiplicator Represent number of indentation(s). By default 1
+	 * @return string
+	 */
+    public function getIndent($multiplicator = 1)
+	{
+		$pad_length = self::INDENTATION_SPACE * $multiplicator;
+		return str_pad('',$pad_length);
+	}
+	
+	/**
 	 * Get preview of generated code
 	 *
 	 * @return string
@@ -54,13 +66,13 @@ abstract class Peak_Codegen
 	}
 	
 	/**
-	 * Need to be overloaded by child but optionnal
+	 * Optionnal, Can be overloaded by child. Will be call each time before generate()
 	 */
 	public function preGenerate() { }
 	
 	/**
 	 * Need to be overloaded by child
 	 */
-	public function generate() { }
+	abstract public function generate();
 	
 }
