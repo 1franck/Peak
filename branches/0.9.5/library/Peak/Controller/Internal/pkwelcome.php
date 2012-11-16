60ac7b5363c1fe0e28f874a97bc0bbdd36928893
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2012-11-16 03:26:10 +0000

    add an app path notification in pkwelcome.php

--- branches/0.9.5/library/Peak/Controller/Internal/pkwelcome.php
+++ branches/0.9.5/library/Peak/Controller/Internal/pkwelcome.php
@@ -38,6 +38,7 @@ class Peak_Controller_Internal_Pkwelcome extends Peak_Controller_Action
 			
 			if(!file_exists($filepath)) {
 				$this->view->intro_warn = 'but your configuration is missing...';
+				$this->view->application_folder_missing = '<br /><small style="color:red">Can\'t find this path :(</small>';
 			}
 			elseif(!in_array($filetype, array('php','ini'))) {
 				$this->view->intro_warn = 'but your configuration file type is not supported...';
@@ -203,7 +204,7 @@ class Peak_Controller_Internal_Pkwelcome extends Peak_Controller_Action
 						<small>Absolute: '.realpath(PUBLIC_ABSPATH).'</small></td>
 				  </tr>
 				  <tr>
-					<td>APPLICATION_ROOT</td>
+					<td>APPLICATION_ROOT{$application_folder_missing}</td>
 					<td>'.APPLICATION_ROOT.'<br />
 						<small>'.realpath(APPLICATION_ABSPATH).'</small></td>
 				  </tr>
