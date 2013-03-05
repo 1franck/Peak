7d8a9effa98a23b2fe60caf0ae442c6d16d3565d
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2013-03-05 04:16:05 +0000

    added method getLanguages() to Peak_Controller_Helper_Request

--- branches/0.9.6/library/Peak/Controller/Helper/request.php
+++ branches/0.9.6/library/Peak/Controller/Helper/request.php
@@ -37,4 +37,35 @@ class Peak_Controller_Helper_Request
         }
     }
 
+    /**
+     * Get parsed $_SERVER['HTTP_ACCEPT_LANGUAGE']
+     * 
+     * @param  boolean $first_only return the first language only
+     * @return array
+     */
+    public function getLanguages($first_only = false)
+    {
+        $result = array();
+
+        if(array_key_exists('HTTP_ACCEPT_LANGUAGE', $_SERVER)) {
+
+            $pattern = '/^(?P<tag>[a-zA-Z]{2,8})'.'(?:-(?P<subtag>[a-zA-Z]{2,8}))?(?:(?:;q=)'.'(?P<quantifier>\d\.\d))?$/';
+
+            foreach(explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']) as $lang) {
+
+                if(preg_match($pattern, $lang, $splits)) {
+                    foreach($splits as $k => $v) {
+                        if(strlen($k) < 2) unset($splits[$k]);
+                    }
+                    $result[] = $splits;
+                }
+
+                if($first_only) break;                
+            }
+        }
+
+        return ($first_only && is_array($result)) ? $result[0] : $result;
+    }
+
+
 }
