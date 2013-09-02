57f499b47ba5b04fd5edcd50374782bf52f0a38e
snake386@hotmail.com <snake386@hotmail.com@c8762f52-3147-8f3a-68e2-4121d4ea683e>
2013-09-02 04:33:00 +0000

    added in class Peak_View_Header methods codeAsStr(), holdOn(), holdOff(), renamed resetHeader() to reset() and finally added some params to methods release() and setRCode()

--- branches/0.9.7/library/Peak/View/Header.php
+++ branches/0.9.7/library/Peak/View/Header.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * View helper - Http header
  *
@@ -22,96 +21,138 @@ class Peak_View_Header
     protected $_content = null;
 
     /**
+     * Did we release header fields
+     * @var boolean
+     */
+    protected $_released = false;
+
+    /**
+     * Hold stop release() method from outputting the stuff
+     * @var boolean
+     */
+    protected $_hold = false;
+
+    /**
      * List of http status codes
      * @var array
      */
     protected $_http_status_codes = array(
 
-        100 => "Continue",
-        101 => "Switching Protocols",
-        102 => "Processing",
-        200 => "OK",
-        201 => "Created",
-        202 => "Accepted",
-        203 => "Non-Authoritative Information",
-        204 => "No Content",
-        205 => "Reset Content",
-        206 => "Partial Content",
-        207 => "Multi-Status",
-        300 => "Multiple Choices",
-        301 => "Moved Permanently",
-        302 => "Found",
-        303 => "See Other",
-        304 => "Not Modified",
-        305 => "Use Proxy",
-        306 => "(Unused)",
-        307 => "Temporary Redirect",
-        308 => "Permanent Redirect",
-        400 => "Bad Request",
-        401 => "Unauthorized",
-        402 => "Payment Required",
-        403 => "Forbidden",
-        404 => "Not Found",
-        405 => "Method Not Allowed",
-        406 => "Not Acceptable",
-        407 => "Proxy Authentication Required",
-        408 => "Request Timeout",
-        409 => "Conflict",
-        410 => "Gone",
-        411 => "Length Required",
-        412 => "Precondition Failed",
-        413 => "Request Entity Too Large",
-        414 => "Request-URI Too Long",
-        415 => "Unsupported Media Type",
-        416 => "Requested Range Not Satisfiable",
-        417 => "Expectation Failed",
-        418 => "I'm a teapot",
-        419 => "Authentication Timeout",
-        420 => "Enhance Your Calm",
-        422 => "Unprocessable Entity",
-        423 => "Locked",
-        424 => "Failed Dependency",
-        424 => "Method Failure",
-        425 => "Unordered Collection",
-        426 => "Upgrade Required",
-        428 => "Precondition Required",
-        429 => "Too Many Requests",
-        431 => "Request Header Fields Too Large",
-        444 => "No Response",
-        449 => "Retry With",
-        450 => "Blocked by Windows Parental Controls",
-        451 => "Unavailable For Legal Reasons",
-        494 => "Request Header Too Large",
-        495 => "Cert Error",
-        496 => "No Cert",
-        497 => "HTTP to HTTPS",
-        499 => "Client Closed Request",
-        500 => "Internal Server Error",
-        501 => "Not Implemented",
-        502 => "Bad Gateway",
-        503 => "Service Unavailable",
-        504 => "Gateway Timeout",
-        505 => "HTTP Version Not Supported",
-        506 => "Variant Also Negotiates",
-        507 => "Insufficient Storage",
-        508 => "Loop Detected",
-        509 => "Bandwidth Limit Exceeded",
-        510 => "Not Extended",
-        511 => "Network Authentication Required",
-        598 => "Network read timeout error",
-        599 => "Network connect timeout error");
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        419 => 'Authentication Timeout',
+        420 => 'Enhance Your Calm',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        424 => 'Method Failure',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        444 => 'No Response',
+        449 => 'Retry With',
+        450 => 'Blocked by Windows Parental Controls',
+        451 => 'Unavailable For Legal Reasons',
+        494 => 'Request Header Too Large',
+        495 => 'Cert Error',
+        496 => 'No Cert',
+        497 => 'HTTP to HTTPS',
+        499 => 'Client Closed Request',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+        598 => 'Network read timeout error',
+        599 => 'Network connect timeout error');
 
 
     /**
      * "Release" header (simply call php header()) and content if any
+     *
+     * @param bool $die 
      */
-    public function release()
+    public function release($die = false)
     {
-        if(!empty($this->_header) && !headers_sent()) {
+        if($this->_hold === true) return;
+
+        if(!empty($this->_header) && !headers_sent() && $this->_released === false) {
+
+            $this->_released = true;
+
             foreach($this->_header as $k => $field) {
                 header($field);
             }
+            if(!is_null($this)) {
+                echo $this->_content;
+            }
+
+            if($die) die();
         }
+    }
+
+    /**
+     * Hold the header relising
+     */
+    public function holdOn()
+    {
+        $this->_hold = true;
+        return $this;
+    }
+
+    /**
+     * Hold the header relising
+     */
+    public function holdOff()
+    {
+        $this->_hold = false;
+        return $this;
     }
 
     /**
@@ -153,25 +194,47 @@ class Peak_View_Header
      * 
      * @return object  $this
      */
-    public function resetHeader()
+    public function reset()
     {
         $this->_content  = '';
         $this->_header[] = array();
+        $this->_released = false;
         return $this;
+    }
+
+    /**
+     * Get http status code text
+     * 
+     * @param  integer $code http status code
+     * @return string
+     */
+    public function codeAsStr($code)
+    {
+        if(array_key_exists($code, $this->_http_status_codes)) {
+            return $this->_http_status_codes[$code];
+        }
+        return;
     }
 
     /**
      * Set a valid http response code
      * 
-     * @param  integer $code
-     * @param  string  $http
+     * @param  integer     $code
+     * @param  bool|string $die   if not false, but a string instead of true, it will die with $die string as message
+     * @param  string      $http
      *
      * @return object  $this
      */
-    public function setRCode($code = 202, $http = 'HTTP/1.1')
+    public function setRCode($code = 202, $die = false, $http = 'HTTP/1.1')
     {
         if(array_key_exists($code,$this->_http_status_codes)) {
             $this->set($http.' '.$code.' '.$this->_http_status_codes[$code]);
+        }
+
+        if($die !== false) {
+            $this->release();
+            if($die === true) $die = null;
+            die($die);
         }
         
         return $this;
@@ -185,11 +248,8 @@ class Peak_View_Header
     public function noCache()
     {
         $h = array(
-            'Expires: Sat, 26 Jul 1997 05:00:00 GMT',
-            'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT',
-            'Cache-Control: no-store, no-cache, must-revalidate',
-            'Cache-Control: post-check=0, pre-check=0', false,
-            'Pragma: no-cache'
+            'Cache-Control: no-cache, must-revalidate',
+            'Expires: Thu, 01 Jan 1970 00:00:00 GMT'
         );
         return $this->set($h);
     }
